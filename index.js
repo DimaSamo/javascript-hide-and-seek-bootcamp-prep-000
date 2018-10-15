6428683daf79fbc68bee6b2433d8776db8f5dcc4
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-15 19:49:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function getFirstSelector(selector){
   return document.querySelector(selector)
 }
+
+function nestedTarget(){
+  var nestedList = querySelectorAll(#nested)
+  return nestedList.querySelectorAll(.target)
+}
