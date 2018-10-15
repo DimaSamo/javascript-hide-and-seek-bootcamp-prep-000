5e864d0da765b31e055cb6a05fd39d4b4c6687e8
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-15 21:01:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,13 @@ function increaseRankBy(n){
   for (let i = 0; i < lis.length; i++) {
   lis[i].innerHTML = parseInt(lis[i].innerHTML)+n
 }
+}
 
+function deepestChild() {
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=0;i<lis.length-1;i++) {
+    test=lis[i].querySelector("div");
+  }
+return test;
 }
