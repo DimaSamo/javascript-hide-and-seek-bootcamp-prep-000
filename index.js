3f591e5c4718002d53997471178bd597fa5d4cfb
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-15 20:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  
 
-  return document.getElementById("nested").getElementByClassName("target")
+
+  return document.getElementById("nested").getElementsByClassName("target")
 }
