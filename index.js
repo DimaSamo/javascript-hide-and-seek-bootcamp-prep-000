38f9f42a0fd4eb3a15fb5fcabbcb12c4e7c4b0e4
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-15 20:52:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget(){
   return document.querySelector("#nested .target")
 }
 function increaseRankBy(n){
-  const lis=.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const lis=getElementById('app').querySelectorAll('ul.ranked-list li')
   for (let i = 0; i < lis.length; i++) {
   lis[i].innerHTML = parseInt(lis[i].innerHTML)+n
 }
